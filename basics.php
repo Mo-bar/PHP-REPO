c099f345f190cec9e1cc0de6731b5db757ce5dfd
mourad_br <mouradbarkouch@gmail.com>
2023-07-09 17:27:11 +0100

    php: convert to boolean

---
+++ basics.php
@@ -0,0 +1,17 @@
+<?php
+########################
+//! Converting to boolean
+
+var_dump((bool) "");
+echo '</br>';
+var_dump((bool) "12");
+echo '</br>';
+var_dump((bool) []);
+echo '</br>';
+var_dump((bool) [12,2,3]);
+echo '</br>';
+var_dump((bool) array());
+echo '</br>';
+var_dump((bool) array(12,34,54));
+
+
