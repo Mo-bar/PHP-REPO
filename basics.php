624afeccaf829beb8a0094550c739ddc23c1c0f2
mourad_br <mouradbarkouch@gmail.com>
2023-07-09 17:27:54 +0100

    php: HereDoc vs NowDoc

--- basics.php
+++ basics.php
@@ -14,4 +14,33 @@ var_dump((bool) array());
 echo '</br>';
 var_dump((bool) array(12,34,54));
 
+########################
+//! HereDoc vs NowDoc
+
+$fname = 'Mourad';
+$lname = 'BARKOUCH';
+$ecole = 'EMSI';
+echo '</br>';
+
+//? Here doc : skip & parse
+echo <<< here
+Nom : $lname,
+Prenom : $fname, 
+Ecole : $ecole
+here;
+echo '</br>';
+echo <<< "here"
+Nom : $lname,
+Prenom : $fname, 
+Ecole : $ecole
+here;
+
+
+echo '</br>';
+//? Here doc : skip only
+echo <<< 'now'
+Nom : $lname,
+Prenom : $fname, 
+Ecole : $ecole
+now;
 
